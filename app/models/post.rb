f7048e04dddda424531576be73b5a35664995833
rilleague <aarima2626@gmail.com>
2022-07-01 16:31:54 +0900

    Postモデル　バリデーションの記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many_attached :images
+
+  with_options presence: true do
+    validates :title
+    validates :genre_id, numericality: { other_than: 0 }
+    validates :detail
+    validates :image
+  end
 end
