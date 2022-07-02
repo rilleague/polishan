aeb4e5993f90f64ecebe20f5a29f6f44255b5347
rilleague <aarima2626@gmail.com>
2022-07-02 17:04:42 +0900

    各モデルにアソシエーションを記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many_attached :images
+  has_many :tags, through: :post_tags, dependent: :destroy
+  has_many :post_tags, dependent: :destroy
 
   with_options presence: true do
     validates :title
