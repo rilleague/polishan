f9543a40d091c413b370b6121035f97cfa655cc7
rilleague <aarima2626@gmail.com>
2022-07-03 23:00:44 +0900

    投稿表示ページの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#index'
+  get 'posts/beautypage' => 'posts#beautypage'
+  get 'posts/troublepage' => 'posts#troublepage'
   resources :users, only: [:edit, :update] 
   resources :posts, only: [:new, :create] 
 end
